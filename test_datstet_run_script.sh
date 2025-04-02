99d8a900eac4516d2dc4abfba394788055b6acf7
Vaishnav Ganapathiraju <vgr976@gmail.com>
2025-04-02 18:51:29 -0500

    modify test script

--- test_datstet_run_script.sh
+++ test_datstet_run_script.sh
@@ -6,10 +6,10 @@
 #SBATCH --mem=2560M                  # Request 2560MB (2.5GB) per node
 #SBATCH --time=12:00:00              # Time limit hh:mm:ss
 #SBATCH --output=%x.log              # Standard output and error log
-module purge
-module laod Anaconda3/2024.02-1
-conda init
-bash
+#module purge
+module load Anaconda3/2024.02-1
+#conda init
+#bash
 conda activate deeplearning_636
 
 python compute_m_heights.py generator_matrices/test_dataset/test_dataset_n9_k4_m5.pkl.gz 2 2
