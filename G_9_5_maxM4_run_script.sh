6b7b4e1602c1c9b52594c25fc99fd5fc9c5914dc
Thesincereghost <vgr976@gmail.com>
2025-04-02 20:31:47 -0500

    Reduce resources in run scripts

--- G_9_5_maxM4_run_script.sh
+++ G_9_5_maxM4_run_script.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=G_9_5_maxM4   # Job name
 #SBATCH --mail-type=END,FAIL         # Mail Events (NONE,BEGIN,FAIL,END,ALL)
 #SBATCH --mail-user=vaishnav.g@tamu.edu   # Replace with your email address
-#SBATCH --ntasks=48                   # Run on a single CPU
-#SBATCH --ntasks-per-node=48 	## Request exactly (or max) of 48 tasks per node
+#SBATCH --ntasks=36                   # Run on a single CPU
+#SBATCH --ntasks-per-node=36 	## Request exactly (or max) of 36 tasks per node
 #SBATCH --mem=36G                 # Request 2560MB (2.5GB) per node
-#SBATCH --time=24:00:00              # Time limit hh:mm:ss
+#SBATCH --time=32:00:00              # Time limit hh:mm:ss
 #SBATCH --output=%x.log              # Standard output and error log
 module load Anaconda3/2024.02-1
 conda activate deeplearning_636
