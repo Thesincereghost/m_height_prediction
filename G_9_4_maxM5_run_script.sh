c2a8552b98131fb122760e811cf3be399bda57f9
Thesincereghost <vgr976@gmail.com>
2025-04-02 19:47:54 -0500

    Fix bug in run scripts

--- G_9_4_maxM5_run_script.sh
+++ G_9_4_maxM5_run_script.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=END,FAIL         # Mail Events (NONE,BEGIN,FAIL,END,ALL)
 #SBATCH --mail-user=vaishnav.g@tamu.edu   # Replace with your email address
 #SBATCH --ntasks=48                   # Run on a single CPU
-#SBATCH --ntasks-per-node=48 	Request exactly (or max) of 48 tasks per node
+#SBATCH --ntasks-per-node=48 	## Request exactly (or max) of 48 tasks per node
 #SBATCH --mem=36G                 # Request 2560MB (2.5GB) per node
 #SBATCH --time=24:00:00              # Time limit hh:mm:ss
 #SBATCH --output=%x.log              # Standard output and error log
