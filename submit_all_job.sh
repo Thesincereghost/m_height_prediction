75796462e3a99bd33aeb42713f15b3cfcff4dbc8
Vaishnav Ganapathiraju <vgr976@gmail.com>
2025-04-02 22:52:45 -0500

    Launched run 3

--- submit_all_job.sh
+++ submit_all_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# sbatch G_10_4_maxM6_run_script.sh
+sbatch G_10_4_maxM6_run_script.sh
 sbatch G_10_5_maxM5_run_script.sh
 sbatch G_10_6_maxM6_run_script.sh
 sbatch G_9_4_maxM5_run_script.sh
